3ff82b39b7830e376677cfd612ff719d39e1709d
kkyingovo <keying8866@gmail.com>
2026-08-21 15:17:33 +0800

    Rename chatbot and add greeting

---
+++ src/main/java/Nicola.java
@@ -0,0 +1,6 @@
+public class Nicola {
+    public static void main(String[] args) {
+        System.out.println("Hello, this is Nicola.");
+        System.out.println("How can I help you?");
+    }
+}
