5de601bdc344efacca2b8aabe1abba89a0de5372
kkyingovo <keying8866@gmail.com>
2026-08-23 04:21:27 +0800

    add mark/unmark

--- src/main/java/Nicola.java
+++ src/main/java/Nicola.java
@@ -14,13 +14,27 @@ public class Nicola {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         String[] tasks = new String[100];
+        boolean[] isDone = new boolean[100];
         int taskCount = 0;
 
         while (!input.equals("bye")) {
             if (input.equals("list")){
+                System.out.println("Here are your tasks babe.");
                 for(int i = 0; i < taskCount; i++){
-                    System.out.println(" " + (i+1) + ". " + tasks[i]);
+                    String status = isDone[i] ? "X" : " ";
+                    System.out.println(" " + (i+1) + ".[" + status + "] " +tasks[i]);
                 }
+            }else if (input.startsWith("mark ")){
+                int index = Integer.parseInt(input.substring(5));
+                isDone[index-1] = true;
+                System.out.println("Good job babe, I'm proud of you.");
+                System.out.println("  [X] " + tasks[index-1]);
+
+            }else if (input.startsWith("unmark ")){
+                int index = Integer.parseInt(input.substring(7));
+                isDone[index-1] = false;
+                System.out.println("Yes babe, I've corrected the mistake.");
+                System.out.println("  [ ] " + tasks[index-1]);
             }else{
                 tasks[taskCount] = input;
                 taskCount++;
